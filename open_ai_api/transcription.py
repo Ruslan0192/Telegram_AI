876b60f7e346372a2386431bcdfc7106bee79612
Ruslan <ruslan.salmin@gmail.com>
2025-02-24 18:28:06 +0300

    all async func

--- open_ai_api/transcription.py
+++ open_ai_api/transcription.py
@@ -1,28 +1,80 @@
+import time
 from pathlib import Path
 
 import config
 
-from openai import OpenAI
+from openai import AsyncOpenAI
 
-client = OpenAI(api_key=config.settings.OPENAI_APIKEY)
+client_async = AsyncOpenAI(api_key=config.settings.OPENAI_APIKEY)
 
 
-# преобразование ИИ голоса из файла в текст
-def def_openai_api_voice_in_text(audio_filename: str):
+# Модуль преобразования ИИ голоса из файла в текст
+# **************************************************************************************
+async def def_openai_api_voice_in_text(audio_filename: str):
     # открываю аудио файл
     with open(audio_filename, "rb") as audio_file:
         # расшифровка
-        transcript = client.audio.transcriptions.create(
+        transcript = await client_async.audio.transcriptions.create(
             model="whisper-1",
             file=audio_file,
+            # temperature=0.5,
+            # response_format="verbose_json"
             # language='ru'
         )
     return transcript.text
 
 
-# преобразование ИИ из текста в файл голосом
-def def_openai_api_text_in_voice(text: str, user_id):
-    response = client.audio.speech.create(
+# Модуль получения ответа от ИИ на вопрос
+# **************************************************************************************
+# создаю ассистента и процесс
+async def def_create_assistant():
+    assistant = await client_async.beta.assistants.create(
+        model="gpt-4-1106-preview",
+        tools=[{"type": "code_interpreter"}],
+    )
+    thread = await client_async.beta.threads.create()
+    return assistant, thread
+
+
+# ожидание ответа
+async def def_get_answer(assistant_id, thread_id):
+    # запуск ассистента
+    run = await client_async.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id=assistant_id
+    )
+
+    # # ожидание ответа
+    while True:
+        runInfo = await client_async.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run.id)
+        if runInfo.completed_at:
+            break
+        # запрос ответа через каждые 0,5 сек
+        time.sleep(0.5)
+
+    # Пришел ответ
+    messages = await client_async.beta.threads.messages.list(thread_id)
+    message_content = messages.data[0].content[0].text.value
+    return message_content
+
+
+# ИИ отвечает на вопрос
+async def def_openai_api_question(assistant, thread, question: str):
+    # добавляю вопрос в процесс
+    await client_async.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=question
+    )
+    # ожидание ответа
+    message_content = await def_get_answer(assistant.id, thread.id)
+    return message_content
+
+
+# Модуль преобразования ИИ из текста в файл голосом
+# **************************************************************************************
+async def def_openai_api_text_in_voice(text: str, user_id):
+    response = await client_async.audio.speech.create(
         model="tts-1",
         voice="alloy",
         input=text,
--- router/user.py
+++ router/user.py
@@ -4,8 +4,11 @@ from aiogram import F, types, Router, Bot
 from aiogram.filters import CommandStart
 from aiogram.types import FSInputFile
 
-from open_ai_api.transcription import def_openai_api_voice_in_text, def_openai_api_text_in_voice
-from open_ai_api.answer_ai import def_openai_api_question
+from open_ai_api.transcription import \
+    def_openai_api_voice_in_text, \
+    def_openai_api_question, \
+    def_openai_api_text_in_voice, \
+    def_create_assistant
 
 user_router = Router()
 
@@ -19,6 +22,9 @@ async def start_cmd(message: types.Message):
 
 @user_router.message(F.voice)
 async def def_get_audio(message: types.Message, bot: Bot):
+    # сообщение об ожидании
+    message_wait = await message.answer('Ожидайте ответ...')
+
     # сохраняю голос. сообщение в файл
     file_id = message.voice.file_id
     file = await bot.get_file(file_id)
@@ -28,28 +34,45 @@ async def def_get_audio(message: types.Message, bot: Bot):
     file_on_disk = f'voice_tmp\{file_id}.wav'
     await bot.download_file(file_path, destination=file_on_disk)
 
-    # отправляю на преобразование ИИ голоса из файла в текст
-    question = def_openai_api_voice_in_text(file_on_disk)
+# отправляю на преобразование ИИ голоса из файла в текст
+    question = await def_openai_api_voice_in_text(file_on_disk)
+    # удаляю сообщение об ожидании
+    await message_wait.delete()
+    # текст вопроса
     await message.reply(question)
 
     # удаляю голосовой файл
     os.remove(file_on_disk)
 
-    # вопрос для ИИ
-    answer = await def_openai_api_question(question)
+# вопрос для ИИ
+    # сообщение об ожидании
+    message_wait = await message.answer('Ожидайте ответ...')
+    # создаю ассистента и процесс
+    assistant, thread = await def_create_assistant()
+    # отправляю вопрос
+    answer = await def_openai_api_question(assistant, thread, question)
+    # удаляю сообщение об ожидании
+    await message_wait.delete()
+    # ответ ИИ
     await message.answer(answer)
 
-    # отправляю на преобразование ИИ из текста в файл голосом
-    file_on_disk = def_openai_api_text_in_voice(answer, message.from_user.id)
+# отправляю на преобразование ИИ из текста в файл голосом
+    # сообщение об ожидании
+    message_wait = await message.answer('Ожидайте ответ...')
+
+    file_on_disk = await def_openai_api_text_in_voice(answer, message.from_user.id)
 
     # записываю файл на диск
     audio_file = FSInputFile(file_on_disk, "Ответ ИИ.wav")
+    # удаляю сообщение об ожидании
+    await message_wait.delete()
+    # отправка голосового файла
     await message.answer_voice(audio_file)
     # удаляю голосовой файл
     os.remove(file_on_disk)
 
 
 @user_router.message()
-async def def_get_audio(message: types.Message):
+async def def_any_message(message: types.Message):
     # обработка ошибок
     await message.answer('Запишите вопрос в голосовом сообщении!')
